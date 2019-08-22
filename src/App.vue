4405e0142552609d9912a6263c883a8eef9047b4
好好学习 天天向上 <wanghuiru@192.168.124.17>
2019-08-22 21:46:10 +0800

    axioss的练习

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,45 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>{{result}}</div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import axios from 'axios'
+import {request} from './network/request'
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    
+  },
+  data(){
+    return{
+      result:''
+    }
+  },
+  created () {
+    // axios({
+    //   url:'http://123.207.32.32:8000/home/multidata'
+    // }).then(res=>{
+    //   this.result=res;
+    // })
+    request({
+  // url:'/home/multidata'
+  url:'/home/data/aaa',
+    params:{
+      type:'sell',
+      page:3
+    }
+}).then(res=>{
+  // console.log(res);
+  this.result=res
+}).catch(err=>{
+  console.log(err); 
+})
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
